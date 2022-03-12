529742bcde82eebc669acac57ada5a331cad8fe2
DRSawPH <101432112+DRSawPH@users.noreply.github.com>
2022-03-12 11:38:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("https://poarchdolls.club/roadmap", "_blank");
                 }}
                 style={{
                   margin: "5px",
